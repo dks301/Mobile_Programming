12243e64206e903583bf3a219b9635079487e187
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2018-03-27 19:24:54 +0900

    LAB1-2

---
+++ LAB1_2/app/src/main/java/org/androidtown/lab1_2/MainActivity.java
@@ -0,0 +1,41 @@
+package org.androidtown.lab1_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    public EditText edit_name;
+
+    public Button btn_print;
+    public Button btn_clear;
+    public TextView view_print;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    public void init() {
+        edit_name = (EditText)findViewById(R.id.editText);
+        btn_print = (Button)findViewById(R.id.printButton);
+        btn_clear = (Button)findViewById(R.id.clearButton);
+        view_print = (TextView)findViewById(R.id.textView);
+    }
+
+    public void clearClicked(View v) {
+        edit_name.setText("");
+        view_print.setText("contents");
+    }
+
+    public void printClicked(View v) {
+        String text= "";
+        text = edit_name.getText().toString();
+        view_print.setText(text);
+    }
+}
